5e11f340a8c8e4a4762c2576ba5d43435cadca4f
fmanteca <fmanteca@ifca.unican.es>
2020-09-30 11:06:01 +0200

    updated

--- doTraining.py
+++ doTraining.py
@@ -15,8 +15,8 @@ from keras.regularizers import l2
 # sys.argv[3]: learning rate 0.00005
 # sys.argv[4]: batch size 1000
 
-#train = pd.read_csv('train.csv')
-train = pd.read_csv('train_fortesting.csv')
+train = pd.read_csv('train.csv')
+#train = pd.read_csv('train_fortesting.csv')
 
 variablesTrain = ["Muon_InnerTrack_eta", "Muon_InnerTrack_phi", "Muon_InnerTrack_charge", "Muon_InnerTrack_pt",  "Muon_TunePTrack_pt", "Muon_DT_s1_nhits","Muon_DT_s1_x_mean","Muon_DT_s1_y_mean","Muon_DT_s1_z_mean","Muon_DT_s1_x_std","Muon_DT_s1_y_std","Muon_DT_s1_z_std","Muon_DT_s1_x_skew","Muon_DT_s1_y_skew","Muon_DT_s1_z_skew","Muon_DT_s1_x_kurt","Muon_DT_s1_y_kurt","Muon_DT_s1_z_kurt","Muon_DT_s2_nhits","Muon_DT_s2_x_mean","Muon_DT_s2_y_mean","Muon_DT_s2_z_mean","Muon_DT_s2_x_std","Muon_DT_s2_y_std","Muon_DT_s2_z_std","Muon_DT_s2_x_skew","Muon_DT_s2_y_skew","Muon_DT_s2_z_skew","Muon_DT_s2_x_kurt","Muon_DT_s2_y_kurt","Muon_DT_s2_z_kurt","Muon_DT_s3_nhits","Muon_DT_s3_x_mean","Muon_DT_s3_y_mean","Muon_DT_s3_z_mean","Muon_DT_s3_x_std","Muon_DT_s3_y_std","Muon_DT_s3_z_std","Muon_DT_s3_x_skew","Muon_DT_s3_y_skew","Muon_DT_s3_z_skew","Muon_DT_s3_x_kurt","Muon_DT_s3_y_kurt","Muon_DT_s3_z_kurt","Muon_DT_s4_nhits","Muon_DT_s4_x_mean","Muon_DT_s4_y_mean","Muon_DT_s4_x_std","Muon_DT_s4_y_std","Muon_DT_s4_x_skew","Muon_DT_s4_y_skew","Muon_DT_s4_x_kurt","Muon_DT_s4_y_kurt"]
 
--- submit.py
+++ submit.py
@@ -5,7 +5,7 @@ import glob
 
 for myfile in glob.glob("send_*sh"):
 
-    subcom = 'sbatch -o logfile.log -e errfile.err --qos=gridui_sort --partition=cloudcms -N 20 ' + myfile
+    subcom = 'sbatch -o logfile.log -e errfile.err --qos=gridui_sort --partition=cloudcms ' + myfile
     os.system(subcom)
 
 
